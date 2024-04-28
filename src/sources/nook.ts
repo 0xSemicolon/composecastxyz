d5a098f6045a76f179abcf8740a937ff1fa69638
0xSemicolon <167597051+0xSemicolon@users.noreply.github.com>
2024-04-29 07:07:32 +1000

    Upgraded nook from a copyPrompt to a redirect source.

--- src/sources/nook.ts
+++ src/sources/nook.ts
@@ -4,9 +4,18 @@ const supercast: ISource = {
     imageUrl: '/images/nook.avif',
     productName: 'Nook',
     domain: 'nook.social',
-    fulfilmentType: 'promptCopy',
-    linkGenerator: () => {
-        return new URL('https://nook.social');
+    fulfilmentType: 'redirect',
+    linkGenerator: (p) => {
+        const url = new URL('https://nook.social/~/compose');
+        if (p.text) {
+            url.searchParams.append('text', p.text);
+        }
+        if (p.embeds?.length) {
+            p.embeds.forEach(e => {
+                url.searchParams.append('embeds[]', e);
+            });
+        }
+        return url;
     },
     preferenceCondition: ({ url }) => /^(.*\.)?nook\.social$/.test(url.hostname)
 };
